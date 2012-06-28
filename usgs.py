98b813d6744e4bec9f909eed1f1803541dd41db8
Tim Dennis <dennis.tim@gmail.com>
2012-06-28 12:09:31 -0700

    adding more todos

--- usgs.py
+++ usgs.py
@@ -14,7 +14,9 @@ SAVE_DIR = "/home/tim/usgs" #think about getting this from cmd line arg
 
 
 def unzip_geofile_and_save(io_input):
-    '''Takes a zip filename, unzips and saves a pdf to the file system'''
+    """
+    Takes a zip filename, unzips and saves a pdf to the file system
+    """
     zf = zipfile.ZipFile(io_input)
     for name in zf.namelist():
         geo_pdf = open(os.path.join(SAVE_DIR, name), 'wb') #look in os for platform independent dir ref
@@ -23,19 +25,22 @@ def unzip_geofile_and_save(io_input):
         geo_pdf.close()
 
 def open_csv_get_urls():
+    """
+    Opens csv and gets urls from file. Returns a list of urls.
+    """
     histtops = csv.DictReader(open(USGS_CSV))
     urllist = list()
     for row in histtops:
         urllist.append(row['DownloadGeoPDF'])
     urllist = random.sample(urllist, 5) #for testing
     geourls = [url.replace(' ', '%20') for url in urllist]
-
     print (geourls)
     return geourls
 
-
 def open_and_unzip_geofiles(geourls):
-    """takes a list of urls, opens, and saves to a file"""
+    """
+    takes a list of urls, opens, and saves to a file
+    """
     for url in geourls:
         response = urllib2.urlopen(url)
         data = response.read()
@@ -53,7 +58,8 @@ if __name__ == '__main__':
 # TODO: context operator
 # TODO: error handling
 # TODO; time how long it takes to download and unzip each one
-# TODO: need to keep track of downloaded files and links, where we are in list of urls, batch running and where to pick back up 
+# TODO: need to keep track of downloaded files and links, where we are in list of urls, batch running and where to pick back up
+# TODO: for above explore popping off the list
 # TODO: poss use pickle to persist tracking
 # TODO: need logging
 # TODO: run as chron
