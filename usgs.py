b8d2782ad1c06fc8b275be52f35465cd5e937dad
Tim Dennis <dennis.tim@gmail.com>
2012-07-03 15:17:23 -0700

    adding logging

--- usgs.py
+++ usgs.py
@@ -3,7 +3,7 @@ import zipfile
 import os
 import urllib2
 import random
-import pickle
+import logging
 
 try:
     from cStringIO import StringIO #faster
@@ -12,8 +12,15 @@ except:
 
 USGS_CSV = "usgshist.csv" #USGS Historical Topo Download
 SAVE_DIR = "/home/tim/usgs" #think about getting this from cmd line arg
-LIST_FILE = "geo_urls"
-
+LAST_LIST_ITEM_FILE = "last_index_processed"
+#setting up logger
+#TODO: find a better way to instantiate logger
+logger = logging.getLogger('usgs-scrape')
+handler = logging.FileHandler('usgs.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
 
 def unzip_geofile_and_save(io_input):
     """
@@ -25,12 +32,12 @@ def unzip_geofile_and_save(io_input):
         geo_pdf.write(zf.read(name))
         geo_pdf.flush()
         geo_pdf.close()
+        logger.info('File %s to directory' %name)
 
 def open_csv_get_urls():
     """
     Opens csv and gets urls from file. Returns a list of urls.
     """
-    #TODO: if picle file exists: open and ruturn list
         #if not empty:
       #      pass
         #else:
@@ -42,31 +49,56 @@ def open_csv_get_urls():
         urllist.append(row['DownloadGeoPDF'])
     urllist = random.sample(urllist, 5) #for testing
     geourls = [url.replace(' ', '%20') for url in urllist]
-
-    print (geourls)
+    #print (geourls)
+    logger.info('Number of urls in file: %d' %len(geouls))
     return geourls
 
-def open_and_unzip_geofiles(geourls):
+def get_start_and_end_index():
+    """
+    If last list index file exists, will read in number as start at +1 of that number and determine end number by adding
+    global bulk load number to determine end index. If last list file doesn't exist, will start at zero. Returns start
+     index and end index.
+    """
+    bulk_run = 20
+    if os.path.isfile(LAST_LIST_ITEM_FILE):
+        with open(LAST_LIST_ITEM_FILE, 'r') as f:
+            start_index = int(f.read())+1
+            stop_index = start_index+bulk_run
+    else:
+        start_index = 0
+        stop_index = start_index+bulk_run
+
+    return start_index, stop_index
+
+
+def save_last_processed_index(last_index):
+    """
+    saves last processed url to a file.
+    """
+    with open(LAST_LIST_ITEM_FILE, 'at') as f:
+        f.write(last_index)
+    logger.info('Set to log last index num.')
+
+def open_and_unzip_geofiles(geourls, start_index, stop_index):
     """
     takes a list of urls, opens, and saves to a file
     """
     #TODO: figure out how to iterate a list and stop at a count
-    count = 0
-    while (count <10):
-        for url in geourls:
-            response = urllib2.urlopen(url)
-            data = response.read()
-            input = StringIO(data)
-            unzip_geofile_and_save(input)
-            geourls.pop(url) #printing or something
-            count += count
-        geourls = pickle.load(open('geo_urls', 'rb'))
-
+    #start = last_index+1
+    #end = last_index+21
+    for index, url in enumerate(geourls[start_index:stop_index]):
+        response = urllib2.urlopen(url)
+        data = response.read()
+        input = StringIO(data)
+        unzip_geofile_and_save(input)
+        if index == stop_index:
+            save_last_processed_index(index)
+            logger.info("")
 
 def main():
     """ main method"""
 
-    open_and_unzip_geofiles(open_csv_get_urls())
+    open_and_unzip_geofiles(open_csv_get_urls(), get_start_and_end_index())
 
 
 if __name__ == '__main__':
@@ -80,8 +112,5 @@ if __name__ == '__main__':
 # TODO: poss use pickle to persist tracking
 # TODO: need logging
 # TODO: run as chron
-#for i in l[:40]:
-#    print i,
-#    print l.pop(0)
 
 
