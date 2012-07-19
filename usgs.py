85f777089661d8e418e35ffc7b112116bc405891
Tim Dennis <dennis.tim@gmail.com>
2012-07-19 16:54:12 -0700

    adding configparser

--- usgs.py
+++ usgs.py
@@ -10,9 +10,9 @@ except:
     from StringIO import StringIO
 from ConfigParser import SafeConfigParser
 
-USGS_CSV = "usgshist.csv" #USGS Historical Topo Download
-CSV_DIR = '/home/tim/Dropbox/usgs-historical-topos/'
-SAVE_DIR = "/home/tim/usgs" #think about getting this from cmd line arg
+#USGS_CSV = "usgshist.csv" #USGS Historical Topo Download
+#CSV_DIR = '/home/tim/Dropbox/usgs-historical-topos/'
+#SAVE_DIR = "/home/tim/usgs" #think about getting this from cmd line arg
 LAST_LIST_ITEM_FILE = "last_index_processed"
 #setting up logger below
 #TODO: find a better way to instantiate logger
@@ -23,40 +23,40 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-def unzip_geofile_and_save(io_input):
+def unzip_geofile_and_save(io_input, save_dir):
     """
     Takes a zip filename, unzips and saves a pdf to the file system
     """
     zf = zipfile.ZipFile(io_input)
     for name in zf.namelist():
-        geo_pdf = open(os.path.join(SAVE_DIR, name), 'wb') #look in os for platform independent dir ref
+        geo_pdf = open(os.path.join(save_dir, name), 'wb') #look in os for platform independent dir ref
         geo_pdf.write(zf.read(name))
         geo_pdf.flush()
         geo_pdf.close()
         logger.info('Saved file %(file)s to %(dir)s' % \
-                    {'file':name, 'dir':SAVE_DIR})
+                    {'file':name, 'dir':save_dir})
 
-def open_csv_get_urls():
+def open_csv_get_urls(csv_dir, usgs_csv):
     """
     Opens csv and gets urls from file. Returns a list of urls.
     """
-    histtops = csv.DictReader(open(CSV_DIR+USGS_CSV))
+    histtops = csv.DictReader(open(csv_dir+usgs_csv))
     urllist = list()
     for row in histtops:
         urllist.append(row['DownloadGeoPDF'])
     urllist = random.sample(urllist, 12) #for testing
     geourls = [url.replace(' ', '%20') for url in urllist]
     logger.info('Number of urls in %(usgs_file)s: %(#)03d' % \
-                {'usgs_file':CSV_DIR+USGS_CSV, '#': len(geourls)})
+                {'usgs_file':csv_dir+usgs_csv, '#': len(geourls)})
     return geourls
 
-def get_start_and_end_index():
+def get_start_and_end_index(bulk_run):
     """
     If last list index file exists, will read in number as start at +1 of that number and determine end number by adding
     global bulk load number to determine end index. If last list file doesn't exist, will start at zero. Returns start
      index and end index.
     """
-    bulk_run = 5 #TODO: move to config
+    #bulk_run = 5 #TODO: move to config
     if os.path.isfile(LAST_LIST_ITEM_FILE):
         with open(LAST_LIST_ITEM_FILE, 'r') as f:
             start_index = int(f.read())
@@ -78,7 +78,7 @@ def save_last_processed_index(last_index):
         f.write(str(last_index))
     logger.info('Index of last url/document process: %s' % last_index)
 
-def open_and_unzip_geofiles(geourls, start_index, stop_index):
+def open_and_unzip_geofiles(geourls, start_index, stop_index, save_dir):
     """
     takes a list of urls, opens, and saves to a file
     """
@@ -86,22 +86,22 @@ def open_and_unzip_geofiles(geourls, start_index, stop_index):
         response = urllib2.urlopen(url)
         data = response.read()
         input = StringIO(data)
-        unzip_geofile_and_save(input)
+        unzip_geofile_and_save(input, save_dir)
     save_last_processed_index(stop_index)
 
 def get_config_info():
     """open usgs.ini and return config"""
     parser = SafeConfigParser()
     parser.read('usgs.ini')
-    return parser.get('config_info',USGS_CSV), parser.get('config_info',CSV_DIR), \
-           parser.get('config_info', SAVE_DIR), parser.getint('config_info', bulk_run)
+    return parser.get('config_info', 'usgs_csv'), parser.get('config_info', 'csv_dir'), \
+           parser.get('config_info', 'save_dir'), parser.getint('config_info', 'bulk_run')
 
 def main():
     """ main method"""
     logger.info("Starting script")
-    usgs_csv, csv_dir, save_dir, bulk_run = get_config_info()#need to refactor above to use
-    start_index, stop_index = get_start_and_end_index()
-    open_and_unzip_geofiles(open_csv_get_urls(), start_index, stop_index)
+    usgs_csv, csv_dir, save_dir, bulk_run = get_config_info() #need to refactor above to use
+    start_index, stop_index = get_start_and_end_index(bulk_run)
+    open_and_unzip_geofiles(open_csv_get_urls(csv_dir, usgs_csv), start_index, stop_index, save_dir)
 
 if __name__ == '__main__':
     main()
